3245628a516fb6d78b4e85dbaa891df29aeafd27
Maoxu Li <li@maoxuli.com>
2018-09-08 22:12:04 -0400

    minor change

--- src/my_image_view/src/image_view_node.cpp
+++ src/my_image_view/src/image_view_node.cpp
@@ -1,11 +1,11 @@
 #include <ros/ros.h>
-//#include "image_view.h"
+#include "image_view.h"
 
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "my_image_view");
     ros::NodeHandle node("~");
-    //ImageView view(node); 
+    ImageView view(node); 
     ros::spin(); 
     return 0; 
 }
