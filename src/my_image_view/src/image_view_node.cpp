857863b68a96847c2547402d35c9ebced75616e6
Maoxu Li <li@maoxuli.com>
2018-09-08 21:56:17 -0400

    delete build folder, add node file

---
+++ src/my_image_view/src/image_view_node.cpp
@@ -0,0 +1,11 @@
+#include <ros/ros.h>
+//#include <my_image_view/image_view.h>
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "my_image_view");
+    ros::NodeHandle node("~");
+    //ImageView view(node); 
+    ros::spin(); 
+    return 0; 
+}
